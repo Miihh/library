d30ce4a4b4c413a4762861c7f817ae85dbf10350
Mihai Blăgniceanu <mihaiblagniceanu@192-168-0-103.rdsnet.ro>
2021-05-07 16:35:18 +0300

    made some progress. form and js

---
+++ script.js
@@ -0,0 +1,21 @@
+var button = document.getElementById("button");
+var title = document.getElementById("title");
+var author = document.getElementById("author");
+var description = document.getElementById("description");
+
+
+
+function addBook() {
+    if (title.value.length > 0 && author.value.length > 0 && description.value.length > 0) {
+    alert("thanks for submiting");
+    } else {
+    alert("insert info mdfk");
+    }
+}
+
+
+
+button.addEventListener('click', addBook);
+
+
+
