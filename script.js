76460f9a12700475a29b466abc5e567e52a68917
Miihh <mihaiblagniceanu@192-168-0-103.rdsnet.ro>
2021-05-09 13:18:45 +0300

    added storage arrays

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
+//Grabbed 
 var button = document.getElementById("button");
 var title = document.getElementById("title");
 var author = document.getElementById("author");
 var description = document.getElementById("description");
+var book = document.getElementById("output");
+
+
+
+// storage arrays
+var titleValue = [];
+var authorValue = [];
+var descriptionValue = [];
 
 
 
 function addBook() {
-    if (title.value.length > 0 && author.value.length > 0 && description.value.length > 0) {
-    alert("thanks for submiting");
-    } 
+    if (title.value.length > 0 
+        && author.value.length > 0 
+        && description.value.length > 0) {
+            displayValues();
+           title.value = ""; 
+           author.value = "";
+           description.value = "";
+    } else {
+        alert("please insert info");
+    }
+}
+
+function displayValues() {
+    titleValue.push(title.value);
+    authorValue.push(authorValue.value);
+    descriptionValue.push(description.value);
+    alert("function is working");
 }
 
 
 
+
 button.addEventListener('click', addBook);
+
+
+
 
 
 
