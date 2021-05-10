c63ccd17f1550f328dfaaedb7790b9a49aa00a02
Miihh <mihaiblagniceanu@192-168-0-101.rdsnet.ro>
2021-05-10 20:14:54 +0300

    added display info function with template literal

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ var button = document.getElementById("button");
 var title = document.getElementById("title");
 var author = document.getElementById("author");
 var description = document.getElementById("description");
-var showBook = document.getElementById("output");
+var showBook = document.querySelector('.table');
 
 
 
-// storage arrays
-var titleValue = [];
-var authorValue = [];
-var descriptionValue = [];
+// // storage arrays
+// var titleValue = [];
+// var authorValue = [];
+// var descriptionValue = [];
 
-// store value in arrays
-function pushValues() {
-    titleValue.push(title.value);
-    authorValue.push(author.value);
-    descriptionValue.push(description.value);
+// // store value in arrays
+// function pushValues() {
+//     titleValue.push(title.value);
+//     authorValue.push(author.value);
+//     descriptionValue.push(description.value);
     
-}
+// }
 
 // check for value input and run storage function
 function addBook() {
     if (title.value.length > 0 
         && author.value.length > 0 
         && description.value.length > 0) {
-            showInfo();
+            createBookEntry();
            title.value = ""; 
            author.value = "";
            description.value = "";
@@ -35,10 +35,17 @@ function addBook() {
 }
 
 
-function showInfo() {
-    alert("your book has been saved");
-}
 
+
+function createBookEntry() {
+    var bookTemplate = 
+    `<tr>
+        <td>${title.value}</td>
+        <td>${author.value}</td>
+        <td>${description.value}</td>
+    </tr>`;
+      showBook.innerHTML += bookTemplate;
+}
 
 
 button.addEventListener('click', addBook);
